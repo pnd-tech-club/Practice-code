21ee305e3f4bbc8c08a66eb82995351479960f6f
matthewknecht <1800887@matthewknecht.local>
2015-02-05 19:17:00 -0600

    Updated to include explainations

--- Python/Simple prompt.py
+++ Python/Simple prompt.py
@@ -1,3 +1,6 @@
+#Asks the user for an input(in this case- their name)
 print 'What is your name?'
+#This prompts and sets the variable "name" as whatever the user inputs
 name = raw_input('> ')
+#Prints the input into printed string(or something like that :P )
 print 'Nice to meet you, %s !' % name
