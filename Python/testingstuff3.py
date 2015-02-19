68dd561bab07daa6c74dd367cbc0b97c089ddab7
matthewknecht <1800887@matthewknecht.local>
2015-02-19 14:04:03 -0600

    Just simple counting down thing(breaks massively when input number is negative)

---
+++ Python/testingstuff3.py
@@ -0,0 +1,12 @@
+#Mainly messnig around with numbers and math
+stop = 0
+stop = stop + 1
+
+def count():
+	global num
+
+num = int(raw_input('# '))
+while stop != num:
+	while num != 0:
+		num = num - 1
+		print num
