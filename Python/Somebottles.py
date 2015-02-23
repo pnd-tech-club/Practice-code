df22710c0abec8b50dfdfba8c80bba5e6a39fbdf
matthewknecht <1800887@matthewknecht.local>
2015-02-23 11:08:34 -0600

    A fun 99 bottles-ish program that lets the user choose how many bottles there are

---
+++ Python/Somebottles.py
@@ -0,0 +1,21 @@
+#Mainly messing around with numbers and math
+#Written in Python 2.something.or.other
+#Written by Matthew Knecht
+stop = 0
+stop = stop + 1
+
+num = int(raw_input('# '))
+
+def count():
+	global num
+
+while stop != num:
+	while num > 1:
+		num = num - 1
+		minus = num - 1
+		print "%r bottles of beer on the wall, %r bottles of beer.  Take one down, pass it around, %r bottles of beer on the wall." % (num, num, minus)
+	if num == 0:
+		quit()
+	elif num < 0:
+		print "lol nope"
+		quit()
