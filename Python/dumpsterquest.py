40a98fc878e6c65eab04c0c753d427737ac31148
matthewknecht <1800887@matthewknecht.local>
2015-03-17 16:09:10 -0500

    Fixed some little annoyances and bugs

--- Python/dumpsterquest.py
+++ Python/dumpsterquest.py
@@ -40,10 +40,10 @@ while stop != 1:
 		x += 1
 	elif act == "w":
 		x -= 1
-	elif act == "num":
+	if act == "num":
 		print x
 		print y
-	elif act == "take":
+	if act == "take":
 		print "What do you want to take?"
 		if x == 0 and y == 0 and torch_true == 0:
 			items = "torch"
@@ -62,14 +62,26 @@ while stop != 1:
 				branch_true = 1
 			else:
 				"You don't see that here."
-	elif act == "look":
-		skip = 0
-	elif act == "inventory" or "inv":
+	if act == "inv":
 		print inventory
+	if act == "look":
+		skip = 0
+	elif act == "quit":
+		print "Are you sure you want to quit? (yes/no)"
+		quit_response = raw_input('> ')
+		if quit_response == "yes":
+			quit()
+		else:
+			skip = 0
+	elif act == "OP420":
+		weapon = 7
 	elif act == "info":
-		print "This game was written by Matthew Knecht in Python 2.7.  It is currently in V0.0.4.  The story of the game revolves around a player who has lost his memory and has to find his way back to his dumpster.  The game is currently not very functional and doesn't have much content- but that will be resolved shortly.  Thanks for playing!"
-	elif act == "help":
-		print "-help \n -look \n -wait \n -inventory(inv) \n -use \n -take \n -move(n,s,e,w) \n -back \n -info"
+		print "Damage: %r\nHealth:\nDefense:" % (damage)
+	elif act == "credits":
+		print "This game was written by Matthew Knecht in Python 2.7.  It is currently in V0.0.7.  The story of the game revolves around a player who has lost his memory and has to find his way back to his dumpster.  The game doesn't have much content- but that will be resolved shortly.  Thanks for playing!"
+	if act == "help":
+		print "-help \n -look \n -wait \n -use \n -take \n -move(n,s,e,w) \n -back \n -info"
+	
 	if x == 0 and y == 0 and torch_true == 0:
 		encounter = 0
 		roominfo = "You have found yourself in a dimly lit cave.  You have no memory of how you got here or who you are.  There is a path to the north and south.  You see a torch on the ground."
@@ -87,9 +99,17 @@ while stop != 1:
 	elif x == 1 and y == 1:
 		roominfo = "You walk to the east and begin to feel the breeze picking up.  You look ahead of you and see a light far ahead."
 		print roominfo
-	elif x == 2 and y == 1:
+	elif x == 2 and y == 1 and branch_true == 0:
 		encounter = 0
-		roominfo = "You reach the end of the tunnel and feel the heat of the sun around you.  The trees tower over you and you hear the sound of rushing water to the south.  You see a good sized tree branch with a pointed end."
+		roominfo = "You reach the end of the tunnel and feel the heat of the sun around you.  The trees tower over you and you hear the sound of rushing water to the north.  You see a good sized tree branch with a pointed end."
+		print roominfo
+	elif x == 2 and y == 1 and branch_true == 1:
+		encounter = 0
+		roominfo = "You reach the end of the tunnel and feel the heat of the sun around you.  The forest to the east glooms over you and you hear the sound of rushing water to the north."
+		print roominfo
+	elif x == 2 and y == 2:
+		encounter = 1
+		roominfo = "There is a swiftly flowing stream here.  To the east is a path to the forest.  You think you see a small cottage far to the north."
 		print roominfo
 #This is used to undo movement into an unexisting room
 	else:
@@ -105,7 +125,21 @@ while stop != 1:
 		damage = 1
 	elif weapon == 1:
 		damage = 3
+	elif weapon == 2:
+		damage = 6
+	elif weapon == 3:
+		damage = 10
+	elif weapon == 4:
+		damage = 15
+	elif weapon == 5:
+		damage = 20
+	elif weapon == 6:
+		damage = 30
+#This weapon is going to be available for debugging through the input of "OP420"
+	elif weapon == 7:
+		damage = 1337
 	stop = 1
-	time += 1
+	act = ""
 	act = raw_input('> ')
+
 	stop = 0
